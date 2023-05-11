3bb4348b04f6677772eb5d4ea12c5fbcd4172bac
Steven Lee <stevelee83@gmail.com>
2023-05-11 12:29:05 -0700

    add all company routes

--- routes/companies.js
+++ routes/companies.js
@@ -1,8 +1,14 @@
-const express = require("express");
+/** Routes for companies */
 
-// const db = require("./fakeDb");
+const express = require("express");
+const { NotFoundError, BadRequestError } = require("../expressError");
+
+
 const router = new express.Router();
 let  db  = require("../db");
+
+
+//TODO: add ORDER BY in sql queries
 
 /**return list of companies */
 router.get("/", async function (req, res) {
@@ -11,73 +17,68 @@ router.get("/", async function (req, res) {
          FROM companies`
          );
   return res.json({
-    "companies": results.rows
+    companies: results.rows
   });
 });
 
-/** return single item */
-router.get("/:name", async function (req, res) {
-  const company = await db.query(
+
+/** return single company */
+router.get("/:code", async function (req, res) {
+  const code = req.params.code;
+  const results = await db.query(
     `SELECT code, name
          FROM companies
-         WHERE`
-         );
-  let returnItem = undefined;
+         WHERE code = $1`, [code]);
+  const company = results.rows[0];
 
-  for (const obj of items) {
-    if (obj.name === name) {
-      returnItem = obj;
-      break;
-    }
-  }
-  return res.json(returnItem);
+  if (!company) throw new NotFoundError(`No matching company: ${code}`);
+  return res.json({ company });
 });
+
 
 /**add item to array and show added item */
-router.post("/", function (req, res) {
-  console.log("body is ", req.body);
-  let item = {
-    "name": req.body.name,
-    "price": req.body.price
-  };
-  items.push(item);
+router.post("/", async function (req, res) {
+  if (req.body === undefined) throw new BadRequestError();
+  const results = await db.query(
+    `INSERT INTO companies (code, name, description)
+         VALUES ($1, $2, $3)
+         RETURNING code, name, description`,
+    [req.body.code, req.body.name, req.body.description]);
+  const company = results.rows[0];
 
-  return res.json({
-    "added": item
-  });
+  return res.status(201).json({ company });
 });
 
 
-/** modify and show modified item */
-router.patch("/:name", function (req, res) {
-  let newName = req.body.name;
-  let newPrice = req.body.price;
-  let currentName = req.params.name;
-
-  for (let obj of items) {
-    if (obj.name === currentName) {
-      obj.name = newName;
-      obj.price = newPrice;
-      return res.json({
-        "Updated": {...obj}
-      });
-    }
+/** update and show updated company */
+router.put("/:code", async function (req, res) {
+  if (req.body === undefined || "code" in req.body) {
+    throw new BadRequestError("Not allowed");
   }
+
+  const code = req.params.code;
+  const results = await db.query(
+    `UPDATE companies
+         SET name=$1, description=$2
+         WHERE code = $3
+         RETURNING code, name, description`,
+    [req.body.name, req.body.description, code]);
+  const company = results.rows[0];
+
+  if (!company) throw new NotFoundError(`No matching company: ${code}`);
+  return res.json({ company });
 });
 
-/** deletes item and returns a message of deleted */
-router.delete("/:name", function (req, res) {
-  let deleteName = req.params.name;
-  for (let obj of items) {
-    if (obj.name === deleteName) {
-      delete obj.name;
-      delete obj.price;
-      obj = null;
-      break;
-    }
-  }
 
-  return res.json({ "message": "Deleted"});
+/** deletes company and returns a status of deleted */
+router.delete("/:code", async function (req, res) {
+  const code = req.params.code;
+  const result = await db.query(`DELETE FROM companies
+                                  WHERE code = $1 RETURNING code`, [code]);
+  const company = result.rows[0]
+
+  if (!company) throw new NotFoundError(`No matching company: ${company}`);
+  return res.json({ status: "deleted" });
 });
 
 
