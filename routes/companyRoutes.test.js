1e6a09af46718a00da349b1bccea517c26476420
Steven Lee <stevelee83@gmail.com>
2023-05-11 17:05:10 -0700

    add some tests for company routes

--- routes/companyRoutes.test.js
+++ routes/companyRoutes.test.js
@@ -2,84 +2,48 @@ const request = require("supertest");
 
 const app = require("../app");
 const db = require("../db");
-// let testItems = [];
 
-let company = {
-  "code": "Pickles",
-  "name": 5.50,
-  "description": "suh bruh"
-};
+let company;
+let invoice;
 
 beforeEach(async function () {
+  await db.query("DELETE FROM companies");
+  await db.query("DELETE FROM invoices")
+  let cResult = await db.query(`
+    INSERT INTO companies (code, name, description)
+    VALUES ('testco', 'Test Company', 'This is a Test')
+    RETURNING code, name, description`);
+  company = cResult.rows[0];
+
+  let iResult = await db.query(`
+    INSERT INTO invoices (comp_code, amt, paid, paid_date)
+    VALUES ('testco', 420.69, FALSE, NULL)
+    RETURNING id`);
+  invoice = iResult.rows[0]
 });
 
-afterEach(function () {
-  items = [];
-});
 
-/** GET /items - returns `[{item}, {item}]` */
-describe("GET /items", function () {
-  it("Gets a list of items", async function () {
-    const resp = await request(app).get(`/items`);
-
-    expect(resp.body).toEqual({ "items":[{
-      "name": "Pickles",
-      "price": 1.50
-    }]});
+/** GET /companies - returns `{companies: , [company,...]}` */
+describe("GET /companies", function () {
+  test("Gets a list of companies", async function () {
+    const resp = await request(app).get(`/companies`);
+    delete company.description;
+    expect(resp.body).toEqual({ companies: [company] });
   });
 });
 
-/** POST /items - create item from data; return `{"added": {name": "Pickles",
-                              "price": 1.50 }}` */
 
-describe("POST /items", function () {
-  it("Creates a new item", async function () {
-    const resp = await request(app)
-      .post(`/items`)
-      .send({
-        "name": "banana",
-        "price": 2.00
-      });
-    expect(resp.statusCode).toEqual(200);
-    expect(resp.body).toEqual({
-      "added": {
-        "name": "banana",
-        "price": 2.00
-      }
-    });
-  });
-});
+/** GET /companies/[code] - return `{company: {company..., invoices: [id...]}}` */
 
-/** PATCH /items/[name] - update item; return `{Updated :{name": "Pickles",
-                              "price": 1.50 }}` */
-
-describe("PATCH /items/:name", function() {
-  it("Updates a single item", async function() {
-    const resp = await request(app)
-      .patch(`/items/Pickles`)
-      .send({
-        "name" : "cucumber",
-        "price" : 3.00
-      });
-    expect(resp.body).toEqual({
-      "Updated" : { "name" : "cucumber",
-      "price" : 3.00}
-    });
+describe("GET /companies/:code", function () {
+  test("Gets single company", async function () {
+    const resp = await request(app).get(`/companies/${company.code}`);
+    company.invoices = [invoice.id]
+    expect(resp.body).toEqual({ company });
   });
 
-  // it("Responds with 404 if name invalid", async function() {
-  //   const resp = await request(app).patch(`/cats/not-here`);
-  //   expect(resp.statusCode).toEqual(404);
-  // });
-});
-
-/** DELETE /items/[name] - delete item,
- *  return `{ "message": "Deleted" }` */
-describe("DELETE /items/:name", function() {
-  it("Deletes a single item", async function() {
-    let resp = await request(app)
-      .delete(`/items/Pickles`);
-    expect(resp.body).toEqual({ "message": "Deleted"});
-    // expect(items.length).toEqual(0);
+  test("404 if not found", async function () {
+    const resp = await request(app).get(`/companies/0`);
+    expect(resp.statusCode).toEqual(404);
   });
 });
